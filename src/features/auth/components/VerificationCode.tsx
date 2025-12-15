be67e215e5df555bc8d53b7c7f992b9864a1329e
gongdlida <jun931223@gmail.com>
2025-12-15 21:17:24 +0900

    refactor: 인증번호 상태와 인증번호 에러메시지 알림 컴포넌트를 App에서 인증번호 컴포넌트로 이동

--- src/features/auth/components/VerificationCode.tsx
+++ src/features/auth/components/VerificationCode.tsx
@@ -1,59 +1,70 @@
 import { Input, Button } from '@/features/common/components'
-import type { VerifyCodeParamsType } from '@/features/auth/type'
+import { MSG_TYPE, type VerifyCodeParamsType } from '@/features/auth/type'
+import { useState } from 'react'
 
 interface VerificationCodeProps {
-  verificationCode: string
   phoneNumber: string
-  setVerificationCode: (verificationCode: string) => void
   isLoading: boolean
-  verifyCode: (params: VerifyCodeParamsType) => Promise<void>
+  onVerifyCode: (params: VerifyCodeParamsType) => Promise<void>
   isVerified: boolean
   verifyCodeError: boolean
   resetVerifyCode: () => void
+  verifyCodeResultMsg: string
 }
 
 export const VerificationCode = ({
-  verificationCode,
-  setVerificationCode,
   isLoading,
   phoneNumber,
-  verifyCode,
+  onVerifyCode,
   isVerified,
   verifyCodeError,
-  resetVerifyCode
+  resetVerifyCode,
+  verifyCodeResultMsg
 }: VerificationCodeProps) => {
+  const [verificationCode, setVerificationCode] = useState('')
   return (
-    <div
-      className='form-group'
-      style={{ display: 'flex', flexDirection: 'column', gap: '1.5rem' }}
-    >
-      <Input
-        label='인증번호:'
-        placeholder='6자리 인증번호'
-        disabled={isLoading}
-        maxLength={6}
-        id='verification-code'
-        type='text'
-        value={verificationCode}
-        onChange={(e) => {
-          if (verifyCodeError) resetVerifyCode()
-          const sanitize = e.target.value.replace(/[^0-9]/g, '')
-          setVerificationCode(sanitize)
-        }}
-      />
+    <>
+      <div
+        className='form-group'
+        style={{ display: 'flex', flexDirection: 'column', gap: '1.5rem' }}
+      >
+        <Input
+          label='인증번호:'
+          placeholder='6자리 인증번호'
+          disabled={isLoading}
+          maxLength={6}
+          id='verification-code'
+          type='text'
+          value={verificationCode}
+          onChange={(e) => {
+            if (verifyCodeError) resetVerifyCode()
+            const sanitize = e.target.value.replace(/[^0-9]/g, '')
+            setVerificationCode(sanitize)
+          }}
+        />
 
-      <Button
-        onClick={() => verifyCode({ code: verificationCode, phoneNumber })}
-        disabled={
-          isLoading ||
-          !isValidCode(verificationCode) ||
-          isVerified ||
-          verifyCodeError
-        }
-        className='verify-button'
-        buttonText={isLoading ? '확인 중...' : '인증번호 확인'}
-      />
-    </div>
+        <Button
+          onClick={() => onVerifyCode({ code: verificationCode, phoneNumber })}
+          disabled={
+            isLoading ||
+            !isValidCode(verificationCode) ||
+            isVerified ||
+            verifyCodeError
+          }
+          className='verify-button'
+          buttonText={isLoading ? '확인 중...' : '인증번호 확인'}
+        />
+      </div>
+      {verifyCodeResultMsg && (
+        <div
+          className={`message ${
+            isVerified ? MSG_TYPE.SUCCESS : MSG_TYPE.ERROR
+          }`}
+        >
+          {verifyCodeResultMsg}
+        </div>
+      )}
+    </>
   )
 }
 
